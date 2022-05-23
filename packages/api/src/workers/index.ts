56fa8145a7c2e7066f975012da8e3e2e7626361a
Octavio Torres <octavio.torres@alphas.technology>
2022-05-23 11:41:23 -0400

    feat(api): crea worker para ensamblar población de una Valoración recién creada.

--- packages/api/src/workers/index.ts
+++ packages/api/src/workers/index.ts
@@ -1,11 +1,29 @@
 
 import * as ms from 'ms';
 
+import CreatePopulationWorker from './individual/create-population';
+
 export default () => {
   setInterval(() => { console.log('Updated at 2022-05-11 14:00'); }, 300000);
-  const exampleInterval = '5m';
+  const minutes = '5m';
 
   setInterval(() => {
-    console.log('Run interval - exampleInterval:', exampleInterval);
-  }, ms(exampleInterval));
+    CreatePopulationWorker.checkCreatePopulation().then((res) => {
+      // tslint:disable-next-line: no-console
+      console.log('StartCreatePopulation exec:', res);
+    }).catch((error) => {
+      // tslint:disable-next-line: no-console
+      console.log('CreatePopulation error:', error);
+    });
+  }, ms(minutes));
+
+  setInterval(() => {
+    CreatePopulationWorker.runCreatePopulation().then((res) => {
+      // tslint:disable-next-line: no-console
+      console.log('RunCreatePopulation exec:', res);
+    }).catch((error) => {
+      // tslint:disable-next-line: no-console
+      console.log('RunCreatePopulation error:', error);
+    });
+  }, ms('2m'));
 };
