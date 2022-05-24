17229c0785f16b16ddef0ad371e7eb3696b4d8f8
Octavio Torres <octavio.torres@alphas.technology>
2022-05-24 18:09:48 -0400

    feat(api): crea workers para lanzamiento y cierre de Valoración, y para envío de correos de lanzamiento y recordatorios.

--- packages/api/src/workers/index.ts
+++ packages/api/src/workers/index.ts
@@ -3,9 +3,12 @@ import * as ms from 'ms';
 
 import CreatePopulationWorker from './individual/create-population';
 import EditPopulationWorker from './individual/edit-population';
+import EvaluationWorker from './individual/check-start-end-dates';
+import EvaluationEmailsWorker from './individual/send-emails';
 
 export default () => {
   setInterval(() => { console.log('Updated at 2022-05-11 14:00'); }, 300000);
+  const quarter = '15m';
   const minutes = '5m';
 
   setInterval(() => {
@@ -41,4 +44,24 @@ export default () => {
       console.log('RunEditingPopulation error:', error);
     });
   }, ms('2m'));
+
+  setInterval(async () => {
+    EvaluationWorker.checkStartEndDates().then((res) => {
+      // tslint:disable-next-line: no-console
+      console.log('EvaluationStartCheckStartEndDates exec:', res);
+    }).catch((error) => {
+      // tslint:disable-next-line: no-console
+      console.log('Evaluation error:', error);
+    });
+  }, ms(quarter));
+
+  setInterval(async () => {
+    EvaluationEmailsWorker.sendEmails().then((res) => {
+      // tslint:disable-next-line: no-console
+      console.log('Send Evaluation Email:', res);
+    }).catch((error) => {
+      // tslint:disable-next-line: no-console
+      console.log('Send Evaluation Email error:', error);
+    });
+  }, ms(minutes));
 };
