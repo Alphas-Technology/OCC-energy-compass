e5fe96a012af144fc4f5e759e8a1974a1c70426d
Octavio Torres <octavio.torres@alphas.technology>
2022-05-25 14:47:57 -0400

    fix(client): estabiliza vista para edición de preguntas de Cuestionario.

--- packages/client/src/views/questionnaires/edit.vue
+++ packages/client/src/views/questionnaires/edit.vue
@@ -11,9 +11,13 @@
           <v-expansion-panels>
             <v-expansion-panel
               v-for="(dimention, ix) in evaluations" :key="ix">
-              <v-expansion-panel-header>{{ $t(`Views.Questionnaires.edit.d_${ dimention }`) }}</v-expansion-panel-header>
+              <!-- Dimension -->
+              <v-expansion-panel-header class="text-uppercase font-weight-bold">
+                {{ $t(`Views.Questionnaires.edit.d_${ dimention }`) }}
+              </v-expansion-panel-header>
               <v-expansion-panel-content>
                 <v-row v-for="(variable, i) in Object.keys(questionnaire.evaluations[dimention])" :key="i">
+                  <!-- Variable -->
                   <v-col cols="12" xs="12">
                     <v-banner
                       color="grey lighten-3"
@@ -24,29 +28,28 @@
                       sticky
                     >{{ $t(`Views.Questionnaires.edit.v_${ variable }`) }}</v-banner>
                   </v-col>
-                  <template v-for="index in 5">
-                    <v-col cols="12" v-for="(question, q) in questionnaire.evaluations[dimention][variable]" :key="index + '-' + q">
-                      <v-tooltip bottom color="blue" >
-                        <template v-slot:activator="{ on }" >
-                          <v-text-field
-                            light outlined
-                            :label="(question.reference || {})[lang]"
-                            name="question"
-                            v-model="(question.question || {})[lang]"
-                            @keypress.enter="sendForm"
-                            @blur="sendForm"
-                            v-on="on"
-                            outline
-                            append-icon="mdi-content-save-edit-outline"
-                            color="blue"
-                          ></v-text-field>
-                        </template>
-                        <span>
-                          {{ $t('Views.Questionnaires.edit.tooltip_info') }}
-                        </span>
-                      </v-tooltip>
-                    </v-col>
-                  </template>
+                  <!-- Questions with Reference -->
+                  <v-col cols="12" v-for="(question, q) in questionnaire.evaluations[dimention][variable]" :key="index + '-' + q">
+                    <v-tooltip bottom color="blue" >
+                      <template v-slot:activator="{ on }" >
+                        <v-text-field
+                          light outlined
+                          :label="(question.reference || {})[lang]"
+                          name="question"
+                          v-model="(question.question || {})[lang]"
+                          @keypress.enter="sendForm"
+                          @blur="sendForm"
+                          v-on="on"
+                          outline
+                          append-icon="mdi-content-save-edit-outline"
+                          color="blue"
+                        ></v-text-field>
+                      </template>
+                      <span>
+                        {{ $t('Views.Questionnaires.edit.tooltip_info') }}
+                      </span>
+                    </v-tooltip>
+                  </v-col>
                 </v-row>
               </v-expansion-panel-content>
             </v-expansion-panel>
