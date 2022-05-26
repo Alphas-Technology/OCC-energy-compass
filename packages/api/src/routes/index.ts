1e2a90ab4d3d7934e816aace6f164c72f6295d47
Octavio Torres <octavio.torres@alphas.technology>
2022-05-26 12:02:13 -0400

    feat(api): crea endpoint para listar y editar Preguntas Abiertas.

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -6,6 +6,7 @@ import EvaluationsRouterFactory from './evaluations';
 import HostsRouterFactory from './hosts';
 import ProductServiceRouterFactory from './product-services';
 import QuestionnairesRouterfactory from './questionnaires';
+import OpenQuestionsRouterfactory from './open-question';
 
 export default (middlewares, services) => {
   const router = Router();
@@ -17,6 +18,7 @@ export default (middlewares, services) => {
   router.use(middlewares.jwt);
   router.use('/product-services', ProductServiceRouterFactory());
   router.use('/questionnaires', QuestionnairesRouterfactory());
+  router.use('/open-questions', OpenQuestionsRouterfactory());
 
   return router;
 };
