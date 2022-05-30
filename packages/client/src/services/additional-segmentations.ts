22d86f2653684c23a1adad961cbc8357eefe193b
Octavio Torres <octavio.torres@alphas.technology>
2022-05-30 17:34:31 -0400

    feat(client): crea servicio para consumir endpoints de Segmentación Adicional.

---
+++ packages/client/src/services/additional-segmentations.ts
@@ -0,0 +1,13 @@
+
+import Services from './base-services'
+
+const service = new Services('additional-segmentation')
+
+export default {
+  list: () => {
+    return service.suiteOperation(() => service.get('list-by-enterprise'))
+  },
+  saveEmployeeDetail: (detailId: number, employeeEnterpriseId: number) => {
+    return service.suiteOperation(() => service.put(`save-employee-detail/${detailId}/${employeeEnterpriseId}`))
+  }
+}
