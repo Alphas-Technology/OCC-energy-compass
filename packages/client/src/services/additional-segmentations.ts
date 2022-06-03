670e8c4fa81173e1eda4cf9545ba324690d8a293
Octavio Torres <octavio.torres@alphas.technology>
2022-06-03 16:17:06 -0400

    feat(client): añade servicio para consumir endpoint de Suite.

--- packages/client/src/services/additional-segmentations.ts
+++ packages/client/src/services/additional-segmentations.ts
@@ -7,6 +7,9 @@ export default {
   list: () => {
     return service.suiteOperation(() => service.get('list-by-enterprise'))
   },
+  listActive: () => {
+    return service.suiteOperation(() => service.get('list-active-by-enterprise'))
+  },
   saveEmployeeDetail: (detailId: number, employeeEnterpriseId: number) => {
     return service.suiteOperation(() => service.put(`save-employee-detail/${detailId}/${employeeEnterpriseId}`))
   }
