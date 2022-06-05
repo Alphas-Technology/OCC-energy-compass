33c1b3781a3d27e04e91e036f1bf813aee9b6563
Octavio Torres <octavio.torres@alphas.technology>
2022-06-05 00:07:41 -0400

    feat(client): completa plantilla y lógica para llenado de Encuesta.

--- packages/client/src/views/evaluations/evaluation.js
+++ packages/client/src/views/evaluations/evaluation.js
@@ -18,21 +18,31 @@ export default Vue.extend({
   },
   data () {
     return {
-      outIntervalDialog: false,
-      dialogIcon: '',
-      dialogText: '',
+      currentPage: 0,
+      pages: [],
+      pageAnswerCnt: {},
+      questionsCount: 1,
+      totalQuestionsCount: 0,
+      hasSegmentation: false,
+      hasAdditionalQuestions: false,
       evaluation: {
         timeZone: '',
         enterprise: {}
       },
-      lang: 0,
+      evaluated: {},
+      answersRefs: {},
+      lang: '',
       completed: false,
+      progress: 0,
+      legendColors: ['#BB3E3E', '#B8663D', '#C2B147', '#B6C144', '#44C156', '#1B5E20'],
+      // Modals
+      outIntervalDialog: false,
+      dialogIcon: '',
+      dialogText: '',
       startDialog: false,
       middleDialog: false,
       helpDialog: false,
       displayedMiddleDialog: false,
-      progress: 0,
-      legendColors: ['#BB3E3E', '#B8663D', '#C2B147', '#B6C144', '#44C156', '#1B5E20'],
       showConfirmation: false
     }
   },
@@ -63,6 +73,11 @@ export default Vue.extend({
       if (this.progress >= 60 && this.progress < 80) return '#B6C144'
       if (this.progress >= 80 && this.progress < 99) return '#44C156'
       return '#1B5E20'
+    },
+    computedQuestionnairePages () {
+      let pg = this.currentPage
+      if (!this.hasSegmentation) pg += 1
+      return pg
     }
   },
   created () {
@@ -70,8 +85,91 @@ export default Vue.extend({
     this.getEvaluation()
   },
   methods: {
-    setProgress () {
-      //
+    changePage () {
+      this.$refs.poll_validation.validate()
+        .then(valid => {
+          if (valid) {
+            this.currentPage++
+            // this.$vuetify.goTo(0, { duration: 1100 })
+            if (this.pageAnswerCnt[this.currentPage] < this.pages[this.currentPage].length) {
+              window.scrollTo({ top: 0, behavior: 'smooth' })
+            }
+          } else {
+            this.$store.dispatch('alert/error', this.$t('Views.Evaluations.evaluation.must_answer_all_questions'))
+          }
+        })
+    },
+    setProgress (isRetake = false) {
+      let answered = 0
+      let qPage = this.hasSegmentation ? 1 : 0
+      for (const key of Object.keys(this.evaluated.temp)) {
+        switch (key) {
+          case 'segmentation':
+            if (this.hasSegmentation) {
+              this.pageAnswerCnt[0] = 0
+              this.evaluated.temp[key].forEach(a => {
+                if (a.detailId) {
+                  answered++
+                  this.pageAnswerCnt[0]++
+                }
+              })
+            }
+            break
+          case 'evaluations':
+            this.evaluated.temp[key].forEach(a => {
+              this.pageAnswerCnt[qPage] = 0
+              a.variable.forEach(v => {
+                if (v.score) {
+                  answered++
+                  this.pageAnswerCnt[qPage]++
+                }
+              })
+              qPage++
+            })
+            break
+          case 'indices':
+            this.pageAnswerCnt[qPage] = 0
+            this.evaluated.temp[key].forEach(a => {
+              if (a.answer) {
+                answered++
+                this.pageAnswerCnt[qPage]++
+              }
+            })
+            break
+          case 'additional':
+            this.pageAnswerCnt[qPage + 1] = 0
+            if (this.hasAdditionalQuestions) {
+              this.evaluated.temp[key].forEach(a => {
+                if (a.answer) {
+                  answered++
+                  this.pageAnswerCnt[qPage + 1]++
+                }
+              })
+            }
+            break
+          case 'open':
+            this.pageAnswerCnt[qPage + 2] = 0
+            this.evaluated.temp[key].forEach(a => {
+              a.answer.forEach(oq => {
+                if (oq) {
+                  answered++
+                  this.pageAnswerCnt[qPage + 2]++
+                }
+              })
+            })
+            break
+        }
+      }
+      const prog = (answered * 100) / this.totalQuestionsCount
+      if (isRetake && prog > 50) {
+        this.displayedMiddleDialog = true
+      }
+      this.progress = prog
+    },
+    showDialog (icon, text) {
+      this.outIntervalDialog = true
+      this.dialogIcon = icon
+      this.dialogText = text
     },
     getEvaluation () {
       this.$store.dispatch('loading/show')
@@ -81,42 +179,167 @@ export default Vue.extend({
             if (res.data.status === 'completed') {
               this.completed = true
               this.$store.dispatch('loading/hide')
-              this.showDialog('/img/expiracion.png', this.$i18n.t('Views.Evaluations.evaluation.expiration_date'))
+              this.showDialog('/img/expiracion.png', this.$t('Views.Evaluations.evaluation.expiration_date'))
             } else {
               this.evaluation = res.data
               const releasedAtParsed = Date.parse(res.data.deliveredAt.split('Z')[0]) / 1000
               const deadLineAtParsed = Date.parse(res.data.validUntil.split('Z')[0]) / 1000
               if (releasedAtParsed > parseInt(Date.now() / 1000)) {
-                this.showDialog('/img/reloj.png', this.$i18n.t('Views.Evaluations.evaluation.before_date'))
+                this.showDialog('/img/reloj.png', this.$t('Views.Evaluations.evaluation.before_date'))
               } else if (deadLineAtParsed < parseInt(Date.now() / 1000)) {
-                this.showDialog('/img/expiracion.png', this.$i18n.t('Views.Evaluations.evaluation.expiration_date'))
+                this.showDialog('/img/expiracion.png', this.$t('Views.Evaluations.evaluation.expiration_date'))
               } else if (res.data.status === 'pending') {
-                this.showDialog('/img/reloj.png', this.$i18n.t('Views.Evaluations.evaluation.not_available'))
+                this.showDialog('/img/reloj.png', this.$t('Views.Evaluations.evaluation.not_available'))
               } else {
-                if (res.evaluated.status === 'completed') {
+                this.evaluated = res.evaluated
+                if (this.evaluated.status === 'completed') {
                   this.completed = true
-                  this.showDialog('/img/expiracion.png', this.$i18n.t('Views.Evaluations.evaluation.evaluation_completed'))
+                  this.showDialog('/img/expiracion.png', this.$t('Views.Evaluations.evaluation.evaluation_completed'))
                 } else {
-                  if (!res.evaluated.sensitiveDataTreatmentPolicyAccepted || !res.evaluated.sensitiveDataTreatmentPolicyAccepted.accepted) {
+                  if (!this.evaluated.sensitiveDataTreatmentPolicyAccepted || !this.evaluated.sensitiveDataTreatmentPolicyAccepted.accepted) {
                     this.startDialog = true
                   }
+                  this.assemblePoll()
                 }
                 this.$store.dispatch('loading/hide')
               }
             }
           } else {
             this.$store.dispatch('loading/hide')
-            this.showDialog('/img/alerta.png', this.$i18n.t('Views.Evaluations.evaluation.invalid_token'))
+            this.showDialog('/img/alerta.png', this.$t('Views.Evaluations.evaluation.invalid_token'))
           }
         }).catch((err) => {
           this.$store.dispatch('alert/error', this.$t(`errors.${err.code}`))
           this.$store.dispatch('loading/hide')
         })
     },
-    showDialog (icon, text) {
-      this.outIntervalDialog = true
-      this.dialogIcon = icon
-      this.dialogText = text
+    assemblePoll () {
+      // Answers Types
+      this.answersRefs = this.evaluation.answersReference
+
+      // Additional Segmentation
+      this.hasSegmentation = Object.keys(this.evaluation.additionalSegmentation).length !== 0
+      if (this.hasSegmentation) {
+        const notAnsweredSeg = !this.evaluated.temp.segmentation.length
+        const seg = []
+        for (const key of Object.keys(this.evaluation.additionalSegmentation)) {
+          this.totalQuestionsCount++
+          seg.push(this.evaluation.additionalSegmentation[key])
+          // Initial answers structure
+          if (notAnsweredSeg) {
+            this.evaluated.temp.segmentation.push({
+              segmentationId: this.evaluation.additionalSegmentation[key].id,
+              detailId: null
+            })
+          }
+        }
+        this.pages.push(seg)
+      }
+
+      // Questionnaire Questions
+      const qQuestions = this.evaluation.questionnaire.evaluations
+      const notAnsweredQuestions = !this.evaluated.temp.evaluations.length
+      let dimCnt = 0
+      for (const dimKey of Object.keys(qQuestions)) {
+        // Initial answers structure
+        if (notAnsweredQuestions) {
+          this.evaluated.temp.evaluations.push({ variable: [] })
+        }
+        const dimensionQuestions = []
+        for (const varKey of Object.keys(qQuestions[dimKey])) {
+          for (const qKey of Object.keys(qQuestions[dimKey][varKey])) {
+            // Initial answers structure
+            if (notAnsweredQuestions) {
+              this.evaluated.temp.evaluations[dimCnt].variable.push({ score: null })
+            }
+            // Questions structure
+            qQuestions[dimKey][varKey][qKey].qCount = this.questionsCount
+            dimensionQuestions.push(qQuestions[dimKey][varKey][qKey])
+            this.questionsCount++
+            this.totalQuestionsCount++
+          }
+        }
+        dimCnt++
+        this.pages.push(dimensionQuestions)
+      }
+
+      // Index Questions
+      this.pages.push(this.evaluation.questionsIndex)
+      this.totalQuestionsCount += this.evaluation.questionsIndex.length
+      if (notAnsweredQuestions) {
+        // Initial answers structure
+        for (const index of this.evaluation.questionsIndex) {
+          this.evaluated.temp.indices.push({ idx: index.idx, answer: null })
+        }
+      }
+
+      // Additional Questions
+      this.hasAdditionalQuestions = this.evaluation.additionalQuestions &&
+        this.evaluation.additionalQuestions.length > 0 &&
+        this.evaluation.additionalQuestions[0].question !== ''
+      if (this.hasAdditionalQuestions) {
+        this.totalQuestionsCount += this.evaluation.additionalQuestions.length
+        this.pages.push(this.evaluation.additionalQuestions)
+        // Initial answers structure
+        if (notAnsweredQuestions) {
+          for (const additionalQ of this.evaluation.additionalQuestions) {
+            this.evaluated.temp.additional.push({ question: additionalQ.question, answer: [null] })
+          }
+        }
+      }
+
+      // Open Questions
+      this.pages.push(this.evaluation.openQuestions)
+      this.totalQuestionsCount += (this.evaluation.openQuestions.length * 3)
+      // Initial answers structure
+      if (notAnsweredQuestions) {
+        for (const openQ of this.evaluation.openQuestions) {
+          this.evaluated.temp.open.push({ question: openQ.question[this.lang], answer: [null, null, null] })
+        }
+      }
+
+      // Set progress when poll is partially filled
+      if (!notAnsweredQuestions) {
+        this.setProgress(true)
+      }
+    },
+    getAnswerRef (idx) {
+      const aRefs = this.answersRefs.find(aRef => aRef.idx === idx)
+      const cleanRef = {}
+      for (const key of Object.keys(aRefs)) {
+        if (!isNaN(parseInt(key))) {
+          cleanRef[key] = aRefs[key]
+        }
+      }
+      return cleanRef
+    },
+    saveAnswers () {
+      this.$store.dispatch('loading/show')
+      evaluationsService.saveEvaluatedTempAnswers(this.$route.params.tokenId, this.evaluated.temp)
+        .then(() => {
+          setTimeout(() => this.setProgress(), 140)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally(() => {
+          this.$store.dispatch('loading/hide')
+        })
+    },
+    finishEvaluatedPoll () {
+      this.$store.dispatch('loading/show')
+      evaluationsService.finishPoll(this.$route.params.tokenId)
+        .then((res) => {
+          this.completed = true
+          this.showDialog('/img/expiracion.png', this.$t('Views.Evaluations.evaluation.evaluation_completed'))
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally(() => {
+          this.showConfirmation = false
+          this.$store.dispatch('loading/hide')
+        })
     }
   }
 })
--- packages/client/src/views/evaluations/evaluation.vue
+++ packages/client/src/views/evaluations/evaluation.vue
@@ -4,7 +4,7 @@
     <v-card v-if="!completed">
       <!-- Header -->
       <v-row justify="center">
-        <v-col cols="12" sm="12" md="8">
+        <v-col cols="12" sm="12" md="8" class="pt-6">
           <section class="enterprise-logo">
             <img
               :src="evaluation.enterprise.logo
@@ -15,9 +15,10 @@
             />
           </section>
         </v-col>
-        <v-col cols="12" sm="8" class="text-center">
+        <v-col cols="12" class="pb-0 text-center">
           <v-card flat>
             <h1 class="display-1">{{ evaluation.displayName || evaluation.name }}</h1>
+            <!--
             <p class="mt-7 mb-2 text-center font-weight-bold">
               {{ $t('Views.Evaluations.evaluation.information_1') }}
             </p>
@@ -28,40 +29,200 @@
               <br/>
               {{ $t('Views.Evaluations.evaluation.information_3') }}
             </p>
+            -->
           </v-card>
         </v-col>
       </v-row>
 
-      <!-- Poll -->
-      <!--
-      <template v-for="(evaluation, i) in questionEvaluation">
-      </template>
-      -->
+      <!-------------------------------------------->
+      <!-------------- Poll Questions -------------->
+      <!-------------------------------------------->
+      <v-card-text>
+        <ValidationObserver ref="poll_validation">
+          <template v-for="(item, i) in pages[currentPage]">
+            <!-- Additional Segmentation -->
+            <div v-if="hasSegmentation && currentPage === 0"
+              :key="`${currentPage}-${i}`"
+            >
+              <v-divider v-if="i > 0" class="mt-4 grey lighten-3"></v-divider>
+              <h5 class="mt-6 text-left headline">
+                {{ item.trans[lang].label }}
+              </h5>
+              <v-row>
+                <v-col cols="12" class="pt-0">
+                  <ValidationProvider rules="required">
+                    <v-radio-group dense hide-details
+                      v-model="evaluated.temp.segmentation[i].detailId"
+                      class="mt-2 mx-4 justify-left"
+                      style="max-width: fit-content;"
+                    >
+                      <v-radio v-for="option in item.details"
+                        :key="option.code"
+                        :value="option.id"
+                        :label="option.trans[lang].label"
+                        color="primary"
+                        @change="saveAnswers()"
+                      ></v-radio>
+                    </v-radio-group>
+                  </ValidationProvider>
+                </v-col>
+              </v-row>
+            </div>
+
+            <!-- Questionnaire Questions -->
+            <div v-else-if="[1,2,3,4].includes(computedQuestionnairePages)"
+              :key="`${currentPage}-${i}`"
+            >
+              <v-divider v-if="i > 0" class="mt-4 grey lighten-3"></v-divider>
+              <h5 class="mt-6 text-left headline">
+                {{ item.qCount }}.-
+                {{ item.question[lang] }}
+              </h5>
+              <v-row>
+                <v-col cols="12" class="pt-0">
+                  <ValidationProvider rules="required">
+                    <v-radio-group dense hide-details
+                      v-model="evaluated.temp.evaluations[computedQuestionnairePages - 1].variable[i].score"
+                      class="mt-2 justify-left"
+                      :class="computedQuestionnairePages > 1 ? 'mx-11' : 'mx-7'"
+                      style="max-width: fit-content;"
+                    >
+                      <v-radio v-for="option in Object.keys(getAnswerRef(item.answers)).sort((a, b) => b - a)"
+                        :key="`aref-${option}`"
+                        :value="parseInt(option)"
+                        :label="getAnswerRef(item.answers)[option][lang]"
+                        color="primary"
+                        @change="saveAnswers()"
+                      ></v-radio>
+                    </v-radio-group>
+                  </ValidationProvider>
+                </v-col>
+              </v-row>
+            </div>
+
+            <!-- Index Questions -->
+            <div v-else-if="computedQuestionnairePages === 5"
+              :key="`${currentPage}-${i}`"
+            >
+              <v-divider v-if="i > 0" class="mt-4 grey lighten-3"></v-divider>
+              <h5 class="mt-6 text-left headline">
+                {{ questionsCount + i }}.-
+                {{ item.question[lang] }}
+              </h5>
+              <v-row>
+                <v-col cols="12" class="pt-0">
+                  <ValidationProvider rules="required">
+                    <v-radio-group dense hide-details
+                      v-model="evaluated.temp.indices[i].answer"
+                      class="mt-2 mx-11 justify-left"
+                      style="max-width: fit-content;"
+                    >
+                      <v-radio v-for="option in Object.keys(getAnswerRef(item.answers)).sort((a, b) => b - a)"
+                        :key="`aref-${option}`"
+                        :value="parseInt(option)"
+                        :label="getAnswerRef(item.answers)[option][lang]"
+                        color="primary"
+                        @change="saveAnswers()"
+                      ></v-radio>
+                    </v-radio-group>
+                  </ValidationProvider>
+                </v-col>
+              </v-row>
+            </div>
+
+            <!-- Additional Questions -->
+            <div v-else-if="hasAdditionalQuestions && computedQuestionnairePages === 6"
+              :key="`${currentPage}-${i}`"
+            >
+              <v-divider v-if="i > 0" class="mt-4 grey lighten-3"></v-divider>
+              <h5 class="mt-6 text-left headline">
+                {{ item.question }}
+              </h5>
+              <v-row>
+                <v-col cols="12" class="pt-0">
+                  <ValidationProvider rules="required">
+                    <v-radio-group dense hide-details
+                      v-model="evaluated.temp.additional[i].answer[0]"
+                      class="mt-2 mx-4 justify-left"
+                      style="max-width: fit-content;"
+                    >
+                      <v-radio v-for="option in item.options"
+                        :key="option"
+                        :value="option"
+                        :label="option"
+                        color="primary"
+                        @change="saveAnswers()"
+                      ></v-radio>
+                    </v-radio-group>
+                  </ValidationProvider>
+                </v-col>
+              </v-row>
+            </div>
+
+            <!-- Open Questions -->
+            <div v-else-if="currentPage === pages.length - 1"
+              :key="`${currentPage}-${i}`"
+            >
+              <h5
+                class="text-left headline"
+                :class="i > 0 ? 'mt-12' : 'mt-6'"
+              >
+                {{ item.question[lang] }}
+              </h5>
+              <v-row class="px-1">
+                <v-col cols="12" md="4"
+                  v-for="oq in [0,1,2]" :key="`w${oq}`"
+                  class="text-left"
+                >
+                  <ValidationProvider rules="required" v-slot="{valid}">
+                    <v-text-field
+                      v-model="evaluated.temp.open[i].answer[oq]"
+                      counter
+                      :maxlength="18"
+                      :placeholder="`${$t('Views.Evaluations.evaluation.answer')} ${oq + 1}`"
+                      @keyup="!valid ? setProgress() : ''"
+                      @blur="valid ? saveAnswers() : setProgress()"
+                    ></v-text-field>
+                  </ValidationProvider>
+                </v-col>
+              </v-row>
+            </div>
+          </template>
+      </ValidationObserver>
+      </v-card-text>
     </v-card>
 
-    <!-- Action Buttons -->
-    <v-row wrap row class="mt-5" justify="center" v-if="$vuetify.breakpoint.xs">
-      <v-col cols="12" class="text-center mt-4">
-        <v-btn width="100%" class="mt-4" color="primary">
+    <!-------------------------------------------->
+    <!-------------- Action Buttons -------------->
+    <!-------------------------------------------->
+    <v-row wrap row justify="center">
+      <v-col cols="12" class="mb-12 mb-sm-0 pt-9 pb-12 pb-sm-6 text-center">
+        <v-btn large
+          v-if="currentPage > 0"
+          color="secondary lighten-3"
+          class="mr-3 px-10"
+          @click="currentPage--"
+        >
           {{ $t('Views.Evaluations.evaluation.input_back') }}
         </v-btn>
-        <v-btn
-          width="100%"
-          class="mt-4"
+
+        <v-btn large
+          v-if="currentPage < pages.length - 1"
           color="primary"
+          class="px-6"
+          @click="changePage()"
         >
           {{ $t('Views.Evaluations.evaluation.input_next') }}
         </v-btn>
-      </v-col>
 
-      <v-col cols="12" sm="6" class="text-center mt-4">
-        <v-btn large
+        <v-btn large v-else
           color="primary"
-          class="mt-4"
+          class="px-6"
           :disabled="progress !== 100"
           @click="showConfirmation = true"
         >
-          {{ $t('Views.Evaluations.evaluation.input_save') }}</v-btn>
+          {{ $t('Views.Evaluations.evaluation.input_finish') }}
+        </v-btn>
       </v-col>
     </v-row>
 
@@ -89,16 +250,25 @@
       :progress="progress"
       @close="() => this.helpDialog = false"
     ></x-help-dialog>
-    <x-info-dialog :dialog="outIntervalDialog" :icon="dialogIcon" :text="dialogText"></x-info-dialog>
+    <x-info-dialog
+      :dialog="outIntervalDialog"
+      :icon="dialogIcon"
+      :text="dialogText"
+    ></x-info-dialog>
     <x-confirmation-modal
       :show.sync="showConfirmation"
       :title="$t('Views.Evaluations.evaluation.confirmation_modal_title')"
-      color="success"
+      :action="finishEvaluatedPoll"
+      color="#1B5E20"
       @close="showConfirmation = false"
     >
-      <template v-slot:question>{{ $t('Views.Evaluations.evaluation.confirmation_modal_des') }}</template>
+      <template v-slot:question>
+        {{ $t('Views.Evaluations.evaluation.confirmation_modal_des') }}
+      </template>
     </x-confirmation-modal>
+
     <x-loading></x-loading>
+    <x-alert></x-alert>
   </v-container>
 </template>
 
