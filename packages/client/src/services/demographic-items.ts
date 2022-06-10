c1327691c962fe417d705cdc735bfe15be334144
Octavio Torres <octavio.torres@alphas.technology>
2022-06-10 11:32:31 -0400

    style(client): corrige lint.

--- packages/client/src/services/demographic-items.ts
+++ packages/client/src/services/demographic-items.ts
@@ -4,7 +4,7 @@ import Services from './base-services'
 const service = new Services('demographic-items')
 
 export default {
-  list: (lang:string) => {
+  list: (lang: string) => {
     return service.suiteOperation(() => service.get('list', { params: { lang: lang } }))
   }
 }
