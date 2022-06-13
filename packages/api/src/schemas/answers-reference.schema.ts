3f8f04708dd731781053c8750eee516bbafd1296
Octavio Torres <octavio.torres@alphas.technology>
2022-06-13 18:07:23 -0400

    feat(api): indexa identificador de opciones de respuesta.

--- packages/api/src/schemas/answers-reference.schema.ts
+++ packages/api/src/schemas/answers-reference.schema.ts
@@ -6,7 +6,7 @@ import { AnswersReference } from '../models/answers-reference';
 export type AnswersReferenceType = AnswersReference & mongoose.Document;
 
 const AnswersReferenceSchema = new mongoose.Schema({
-  idx: Number,
+  idx: { index: true, type: Number, unique: true },
   5: { es: String, en: String },
   4: { es: String, en: String },
   3: { es: String, en: String },
