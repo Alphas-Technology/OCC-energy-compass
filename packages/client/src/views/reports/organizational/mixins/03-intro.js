b32014e89492705f0c0912c38a8c07af16579794
Octavio Torres <octavio.torres@alphas.technology>
2022-06-23 12:11:37 -0400

    feat(client): incluye contenido de páginas estáticas del Reporte Organizacional.

--- packages/client/src/views/reports/organizational/mixins/03-intro.js
+++ packages/client/src/views/reports/organizational/mixins/03-intro.js
@@ -12,30 +12,58 @@ export default {
       return [
         // Page Title
         pdfUtils.generateHeaderTitle(this.$t('Views.Evaluations.report.toc.introduction')),
-        // Examples
+        { text: this.$t('Views.Evaluations.report.introduction.first_paragraph'), margin: [10, 50, 0, 0], alignment: 'justify' },
+        { text: this.$t('Views.Evaluations.report.introduction.second_paragraph'), margin: [10, 0, 0, 0], alignment: 'justify' },
+        { text: this.$t('Views.Evaluations.report.toc.model').toUpperCase(), margin: [10, 15, 10, 0], fontSize: 14, bold: true },
+        { text: this.$t('Views.Evaluations.report.introduction.third_paragraph'), margin: [10, 12, 0, 0], alignment: 'justify' },
+        { text: this.$t('Views.Evaluations.report.introduction.fourth_paragraph'), margin: [10, 0, 0, 0], alignment: 'justify' },
         {
-          image: ChipBlueBase64,
-          fit: [0.1, 0.1],
-          margin: [0, 0, 0, 0],
-          alignment: 'center'
+          layout: 'noBorders',
+          margin: [-8, 0, 0, 0],
+          table: {
+            body: [
+              [
+                {
+                  image: ChipBlueBase64,
+                  fit: [350, 200],
+                  margin: [0, 0, 0, 0],
+                  alignment: 'center'
+                },
+                { text: this.$t('Views.Evaluations.report.introduction.professional'), margin: [-320, 22, 0, 0], fontSize: 13, bold: true },
+                {
+                  image: ChipGreenBase64,
+                  fit: [350, 200],
+                  margin: [0, 0, 0, 0],
+                  alignment: 'center'
+                },
+                { text: this.$t('Views.Evaluations.report.introduction.physical'), margin: [-320, 22, 0, 0], fontSize: 13, bold: true }
+              ]
+            ]
+          }
         },
         {
-          image: ChipGreenBase64,
-          fit: [0.1, 0.1],
-          margin: [0, 0, 0, 0],
-          alignment: 'center'
-        },
-        {
-          image: ChipGreyBase64,
-          fit: [0.1, 0.1],
-          margin: [0, 0, 0, 0],
-          alignment: 'center'
-        },
-        {
-          image: ChipRedBase64,
-          fit: [0.1, 0.1],
-          margin: [0, 0, 0, 0],
-          alignment: 'center'
+          layout: 'noBorders',
+          margin: [-8, 0, 0, 0],
+          table: {
+            body: [
+              [
+                {
+                  image: ChipGreyBase64,
+                  fit: [350, 200],
+                  margin: [0, -10, 0, 0],
+                  alignment: 'center'
+                },
+                { text: this.$t('Views.Evaluations.report.introduction.emotional'), margin: [-320, 12, 0, 0], fontSize: 13, bold: true },
+                {
+                  image: ChipRedBase64,
+                  fit: [350, 200],
+                  margin: [0, -10, 0, 0],
+                  alignment: 'center'
+                },
+                { text: this.$t('Views.Evaluations.report.introduction.mental'), margin: [-320, 12, 0, 0], fontSize: 13, bold: true }
+              ]
+            ]
+          }
         }
       ]
     }
--- packages/client/src/views/reports/organizational/mixins/05-model.js
+++ packages/client/src/views/reports/organizational/mixins/05-model.js
@@ -1,6 +1,8 @@
 
 import pdfUtils from '../../utils/pdf'
 
+import ModelBase64 from '../../base64Files/model'
+
 export default {
   methods: {
     $generateModelDescription () {
@@ -8,7 +10,10 @@ export default {
         // Page Title
         pdfUtils.generateHeaderTitle(this.$t('Views.Evaluations.report.toc.model')),
         {
-          text: ''
+          image: ModelBase64,
+          fit: [730, 760],
+          margin: [0, 40, 0, 0],
+          alignment: 'center'
         }
       ]
     }
